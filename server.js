8260a968970ddf88499c2a0ab8351f0f7cb973b0
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-02 19:33:05 +0000

    Adding delete request

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
-const json = require('./Develop/db/db.json');
+const fs = require('fs');
+let json = require('./Develop/db/db.json');
 const shortid = require('shortid');
 
 // Sets up the Express App
@@ -22,14 +23,27 @@ app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname
 app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/assets/js/index.js')));
 
 app.get('/api/notes', (req, res) => {
-    res.json(json);
-  });
+  res.json(json);
+});
 
-  app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = shortid.generate();
-    json.push(newNote)
-  });
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = shortid.generate();
+  json.push(newNote);
+  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(json));
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+  const noteId = (req.params.id).toString();
+
+  json = json.filter(CurrentNote => {
+    return CurrentNote.id != noteId;
+  })
+
+  //write the updated data to db.json and display the updated note
+  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(json));
+  res.send(`Deleting note with id ${noteId}`)
+})
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
