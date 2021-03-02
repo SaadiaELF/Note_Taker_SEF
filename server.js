8ebb1c9aace587120c7df173624d2af195b26d61
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-02 17:39:00 +0000

    Changing paths

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
-const json = require('./db/db.json');
+const json = require('./Develop/db/db.json');
 
 // Sets up the Express App
 const app = express();
@@ -12,13 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
 
-app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'assets/css/styles.css')));
+app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/assets/css/styles.css')));
 
-app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'assets/js/index.js')));
+app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/assets/js/index.js')));
 
 app.get('/api/notes', (req, res) => {
     res.json(json);
